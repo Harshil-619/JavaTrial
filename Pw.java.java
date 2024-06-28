4f9a0d46e7701dd642997334b9309c189e6538b9
Harshil-619 <harshilb619@gmail.com>
2024-06-28 13:34:30 +0530

    Modified Pw.java

--- Pw.java.java
+++ Pw.java.java
@@ -2,5 +2,7 @@ Class Pw.java {
 public static void main (String args[]) {
 System.out.println ("Hello Java");
 System.out.println ("Hello Java");
+System.out.println ("Hello Java");
+System.out.println ("Hello Java");
 }
 }
