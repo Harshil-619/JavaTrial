96a425821b45994fc73e482f43fbc5309e87b54f
Harshil-619 <harshilb619@gmail.com>
2024-06-28 17:32:09 +0530

    New file added

---
+++ Experiment.java
@@ -0,0 +1,8 @@
+Class Pw.java {
+public static void main (String args[]) {
+System.out.println ("Hello Java");
+System.out.println ("Hello Java");
+System.out.println ("Hello Java");
+System.out.println ("Hello Java");
+}
+}
